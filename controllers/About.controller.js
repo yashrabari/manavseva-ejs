a542868af5c6fbc2dab8bba565cad4ba5e57b576
Biren00 <birenbhalodiya19@gmail.com>
2023-04-12 19:00:56 +0530

    try to fix routs problem

--- controllers/About.controller.js
+++ controllers/About.controller.js
@@ -1,31 +1,29 @@
-
-const axios = require('axios')
-
-
-
-
+const axios = require("axios");
 
 async function getRoute(req, res) {
-    var testimonials = await axios.get("http://185.208.206.231:8899/api/testimonials?populate=*", {
-        headers: {
-            Authorization: "Bearer " + process.env.API_TOKEN
-        }
-    })
-    var gallery = await axios.get("http://185.208.206.231:8899/api/gallery?populate=*", {
-        headers: {
-            Authorization: "Bearer " + process.env.API_TOKEN
-        }
-    })
-    res.render("about", {
-        testimonials: testimonials.data.data,
-        gallery: gallery.data.data.attributes.gallery.data,
-        route: 'about'
-    })
+  var testimonials = await axios.get(
+    "http://185.208.206.231:8899/api/testimonials?populate=*",
+    {
+      headers: {
+        Authorization: "Bearer " + process.env.API_TOKEN,
+      },
+    }
+  );
+  var gallery = await axios.get(
+    "http://185.208.206.231:8899/api/gallery?populate=*",
+    {
+      headers: {
+        Authorization: "Bearer " + process.env.API_TOKEN,
+      },
+    }
+  );
+  res.render("about", {
+    testimonials: testimonials.data.data,
+    gallery: gallery.data.data.attributes.gallery.data,
+    route: "about",
+  });
 }
-
-
-
 
 module.exports = {
-    getRoute,
-}
+  getRoute,
+};
